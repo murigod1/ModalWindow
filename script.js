607e4ba5960e28eea010ef61d44afd2cb231ada4
Murilo Jesus <murilexfirebr@gmail.com>
2023-02-23 21:02:42 -0300

    Adition of the classes that will be manipulated, later

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const hiddenBox = document.querySelector('.hidden');
+const boxOverlay = document.querySelector('.overlay');
