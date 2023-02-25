67e25ba4c492994eda7efe70cdcb1e55c87ab277
Murilo Jesus <murilexfirebr@gmail.com>
2023-02-24 21:24:12 -0300

    Finished Modal

--- script.js
+++ script.js
@@ -3,3 +3,29 @@
 const btnsShowModal = document.querySelectorAll('.show-modal');
 const hiddenBox = document.querySelector('.hidden');
 const boxOverlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const xModal = document.querySelector('.close-modal');
+
+const openModal = function(){
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    boxOverlay.classList.remove('hidden');
+}
+const closeModel = function(){
+    modal.classList.add('hidden');
+    boxOverlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModal.length; i++)
+btnsShowModal[i].addEventListener('click', openModal)
+
+xModal.addEventListener('click', closeModel)
+
+boxOverlay.addEventListener('click', closeModel)
+
+document.addEventListener('keydown', function(e){
+    console.log(e.key)
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModel();
+        }
+    })
